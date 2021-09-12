cade7d3b304c317f17fae1ceae6ba08585633cf9
Akshat Pal <aksha@X8sMachine.akshat>
2021-09-12 16:18:03 +0530

    interface added

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
-console.info('app.jsIsConnected')
+let username = document.getElementById('username')
+let name = document.getElementById('name')
+let password = document.getElementById('password')
+let email = document.getElementById('email')
+let address = document.getElementById('address')
+let submit = document.getElementById('submit')
+
+submit.addEventListener('click', ()=>{
+    console.log('buttonClicked')
+})
